aa7301c6b9b6b845df673d9faec16b53b6a5261a
Sho <sho.weblife@gmail.com>
2020-01-08 18:07:48 +0900

    Initial Commit

---
+++ TestComment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestComment
+//
+//  Created by Shota Ito on 2020/01/08.
+//  Copyright © 2020 Shota Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
